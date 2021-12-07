08f165d571ca0feace17f56c5bbb308f0a49e160
Alex Truong <Alex@Alexs-MacBook-Pro-3.local>
2021-12-07 10:29:48 -0800

    Express server set-up with initital routes

---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router()
+
+
+
+//@route    GET    api/auth
+//@desc     Get logged in user
+//@acess    Private 
+
+router.get("/", (req, res) => {
+    res.send("Get loggined in user")
+});
+
+
+//@route    POST    api/auth
+//@desc     Auth user & Get token
+//@acess    Private 
+
+router.post("/", (req, res) => {
+    res.send("Log in user")
+});
+
+
+
+
+
+
+
+module.exports = router;
