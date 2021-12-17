46f2b4e74c7f07ac0246d743760825cb2fb25ffa
Alex Truong <Alex@Alexs-MacBook-Pro-3.local>
2021-12-16 23:16:50 -0800

    fixing env routes

--- server.js
+++ server.js
@@ -20,11 +20,12 @@ app.use("/api/contacts", require("./routes/contacts"))
 
 
 // Serve static assets in production 
-if (process.emitWarning.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     // Set static folder
     app.use(express.static("client/build"))
 
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "indext.html")))
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "indext.html")))
 
 }
 
